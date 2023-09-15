e4139f66eb794e1965d02f26d6d3a62b999a2b4c
Paul Zijlmans <paulzijlmans@hotmail.com>
2023-09-15 09:13:03 +0200

    open and close the modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent {
+  constructor(public modal: ModalService) {}
 
+  closeModal() {
+    this.modal.toggleModal();
+  }
 }
