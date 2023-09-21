0477ad9cfdcc554e30ef38de7f2f3322ca76989c
Paul Zijlmans <paulzijlmans@hotmail.com>
2023-09-21 15:30:00 +0200

    Reactive Forms - Register

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, provideEnvironmentNgxMask } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
+import { InputComponent } from './input/input.component';
 import { ModalComponent } from './modal/modal.component';
 import { TabComponent } from './tab/tab.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+  ],
+  providers: [provideEnvironmentNgxMask()],
 })
 export class SharedModule {}
